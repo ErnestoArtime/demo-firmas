54b08da6a7340e7e9ff2801ce1a9cccfe4689d6f
Ernesto Artime <ernest.av90@gmail.com>
2026-02-19 21:56:35 -0500

    swagger

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,9 +4,15 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public RedirectView redirectToSwagger() {
+        return new RedirectView("/swagger-ui/index.html");
+    }
 
     @GetMapping("/hola")
     public String hola() {
---
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Firmas y Plantillas")
+                        .version("1.0")
+                        .description("Documentación de los endpoints para la gestión de documentos y plantillas.")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
